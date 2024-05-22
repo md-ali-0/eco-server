8c05a6d38644fa0bd803773bd2b110854008da9d
md-ali-0 <mohammad.98482@gmail.com>
2024-05-22 15:37:53 +0600

    Order Route Added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { OrderRouter } from "../modules/order/order.route";
 import { ProductsRoute } from "../modules/product/product.route";
 
 const router = Router()
 
 router.use('/products', ProductsRoute)
+router.use('/products', OrderRouter)
 
 export default router
